3bada3f8e548e5be4802b1a2fa3b9d55dcd443cf
devdynam0507 <wsnam0507@naver.com>
2024-05-26 10:39:12 +0900

    feat: api 연동

---
+++ ormi-html-3/js/gallery.js
@@ -0,0 +1,26 @@
+const API_ENDPOINT = 'https://picsum.photos/v2/list';
+
+function createGalleryImage(url) {
+    const div = document.createElement('div');
+    div.className = 'shadow-box gallery-img-box';
+    div.style.backgroundImage = `url("${url}")`;
+    console.log(url);
+
+    return div;
+}
+
+function render(data, containerId) {
+    const container = document.getElementById(containerId);
+
+    data.forEach(v => {
+        const imageBox = createGalleryImage(v.download_url);
+        container.appendChild(imageBox);
+    })
+}
+
+async function fetchMoreGalleryPictures(containerId, limit) {
+    const response = await fetch(`https://picsum.photos/v2/list?page=1&limit=${limit}`);
+    const data = await response.json();
+
+    render(data, containerId);
+}
